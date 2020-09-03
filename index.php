71167e7e7df668fff01c6bf427728e6f244498e2
luksk <lukskill@hotmail.com>
2020-09-03 01:29:01 -0300

    Funcionalidade raiz quadrada implementada

--- index.php
+++ index.php
@@ -9,6 +9,30 @@ $climate = new League\CLImate\CLImate;
     $climate->out('[3] Ao Quadrado');
 
     $input = $climate->input('Escolha uma das opções acima: ');
-    $input->accept(['1', '2', '3', '4']);
+    $input->accept(['1', '2', '3']);
     $menu = $input->prompt();
+
+    switch ($menu) {
+        case '1':
+            //raiz quadrada
+            $numeroRaizInput = $climate->input('Insira o número: ');
+            $numeroRaiz = $numeroRaizInput->prompt();
+
+            echo sprintf("Resultado: %s", sqrt($numeroRaiz));
+            break;
+        
+        case '2':
+            //Fatorial
+
+            break;
+        
+        case '3':
+            //Ao Quadrado
+
+            break;
+        
+        default:
+            # code...
+            break;
+    }
 
