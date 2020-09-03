4f534fcec45d9e48cbfc288ec27e1e3f6fa8f8ce
luksk <lukskill@hotmail.com>
2020-09-03 01:13:17 -0300

    Criando opções de saida no menu via terminal

--- index.php
+++ index.php
@@ -4,5 +4,11 @@ require_once('vendor/autoload.php');
 
 $climate = new League\CLImate\CLImate;
 
-$climate->out('Escrever no terminal');
+    $climate->out('[1] Raiz Quadrada');
+    $climate->out('[2] Fatorial');
+    $climate->out('[3] Ao Quadrado');
+
+    $input = $climate->input('Escolha uma das opções acima: ');
+    $input->accept(['1', '2', '3', '4']);
+    $menu = $input->prompt();
 
